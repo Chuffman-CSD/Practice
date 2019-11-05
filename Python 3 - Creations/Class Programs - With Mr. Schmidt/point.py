cf41d102cd13d90fa6ac6eae0ff9de86300dd145
Chuffman-CSD <chuffman@stcharlessd.org>
2019-11-05 13:43:21 -0600

    modified:   Python 3 - Creations/Class Programs - With Mr. Schmidt/point.py

--- Python 3 - Creations/Class Programs - With Mr. Schmidt/point.py
+++ Python 3 - Creations/Class Programs - With Mr. Schmidt/point.py
@@ -4,4 +4,18 @@
 
 ''' point.py is a program that will make a cartesian graph'''
 
-class 
+class Point(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        msg = "(" + str(self.x) + " , "
+        msg += str(self.y) + ")"
+        return msg
+        
+
+if __name__ == "__main__":
+    test_point = Point(4,6)
+    print(test_point)
